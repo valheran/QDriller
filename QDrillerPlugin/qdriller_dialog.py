a5feb331028e6b2e6b5fb1ec8fb9de0cffccc32e
valheran <valheran@gmail.com>
2015-04-09 07:12:26 +1000

    Generate Section GUI, Backend
    More linking up of the Generate Section GUI (drillhole and log selection
    boxes.  Also tested the running of the drawing functions. Implemented
    saving project settings to file and then reading them back in. still
    need to implement adding section info to this file

--- QDrillerPlugin/qdriller_dialog.py
+++ QDrillerPlugin/qdriller_dialog.py
@@ -22,7 +22,11 @@
 """
 
 import os
-
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+    
 from PyQt4 import QtGui, uic, QtCore
 from PyQt4.QtGui import * 
 
@@ -62,10 +66,10 @@ class QDrillerDialog(QtGui.QMainWindow, FORM_CLASS):
         self.btnColbrowse.clicked.connect(lambda:self.showFileBrowser("collarfile"))
         self.btnCreatePrj.clicked.connect(self.createProject)
         self.btnDHlogbrowse.clicked.connect(lambda:self.showFileBrowser("logfile"))
-        self.btnLoadPrj.clicked.connect(self.printout)
+        self.btnLoadPrj.clicked.connect(self.loadProject)
         self.btnPrjDirbrowse.clicked.connect(lambda:self.showFileBrowser("projdir"))
         self.btnRemlog.clicked.connect(self.removefromLoglist)
-        #self.btnSavePrj.clicked.connect()
+        self.btnSavePrj.clicked.connect(self.saveProject)
         self.btnSurvbrowse.clicked.connect(lambda:self.showFileBrowser("surveyfile"))
         self.btnCRS.clicked.connect(self.fetchCRS)
         
@@ -92,8 +96,9 @@ class QDrillerDialog(QtGui.QMainWindow, FORM_CLASS):
         
         ##Other Signals###
         QDrillerDialog.datastore.fileCreated.connect(self.addtoLayerList)
+        QDrillerDialog.datastore.projectLoaded.connect(self.onProjectLoad)
         
-        #functions for running the gui
+    #functions for running the gui
         
     def showFileBrowser(self,target):
     #call up a file browser with filter for extension. 
@@ -112,6 +117,14 @@ class QDrillerDialog(QtGui.QMainWindow, FORM_CLASS):
         elif target == "logfile":
             self.ledDHlogs.setText(QtGui.QFileDialog.getOpenFileName(self, 'Select Downhole Log File', rootdir, "*.csv"))
             
+    def showSaveFile(self):
+        if QDrillerDialog.datastore.projectdir is not None:
+            rootdir = QDrillerDialog.datastore.projectdir
+        else:
+            rootdir = "/"
+            
+        QDrillerDialog.datastore.savefilepath = (QtGui.QFileDialog.getSaveFileName(self, 'Save Project Definition', rootdir, "*.qdsv"))
+        
     def setVariable(self, target, value):
         
         if target == "collarfile":
@@ -156,7 +169,7 @@ class QDrillerDialog(QtGui.QMainWindow, FORM_CLASS):
         self.refreshGui()
         
     
-    def addtoLayerList(self, ):
+    def addtoLayerList(self):
         self.lstExistingLayers.clear()
         for keys in QDrillerDialog.datastore.existingLayersDict:
             newitem = self.lstExistingLayers.addItem(keys)
@@ -193,9 +206,39 @@ class QDrillerDialog(QtGui.QMainWindow, FORM_CLASS):
         self.cmbLogSelector.addItems(QDrillerDialog.datastore.logfiles)
         
     def createProject(self):
-        self.gpbxPlanview.setEnabled(True)
-        QDrillerDialog.datastore.createProjectFile()
+        
+        print "project created"
+         
+        self.showSaveFile()
+        QDrillerDialog.datastore.saveProjectData()
         QDrillerDialog.datastore.calcDrillholes()
+        self.gpbxPlanview.setEnabled(True)
+        # also set up enabling the save button and the sectionview 
+        
+    def saveProject(self):
+        QDrillerDialog.datastore.saveProjectData()
+        
+    def loadProject(self):
+        QDrillerDialog.datastore.savefilepath = (QtGui.QFileDialog.getOpenFileName(self, 'Open from Project Definition', "/", "*.qdsv"))
+        QDrillerDialog.datastore.loadProjectData()
+        
+    def onProjectLoad(self):
+        self.ledCol.setText(QDrillerDialog.datastore.collarfile)
+        self.ledProjDir.setText(QDrillerDialog.datastore.projectdir)
+        self.ledSur.setText(QDrillerDialog.datastore.surveyfile)
+        self.ledProjName.setText(QDrillerDialog.datastore.projectname)
+        self.ledCRS.setText(QDrillerDialog.datastore.projectCRS.authid())
+        self.addtoLayerList()
+        self.lstDHlogs.clear()
+        for keys in QDrillerDialog.datastore.logfiles:
+            newitem = self.lstDHlogs.addItem(keys)
+        self.refreshGui()
+        #consider working out a more efficient way of having the drillhole data ready
+        # to use, but not needing to be calculated if its not used
+        QDrillerDialog.datastore.calcDrillholes()
+        
+        #enable buttons and planview groupbox
+        self.gpbxPlanview.setEnabled(True)
         
     def fetchCRS(self):
         #call on the QGIS GUI CRS selection Dialog
@@ -222,6 +265,7 @@ class QDrillerDialog(QtGui.QMainWindow, FORM_CLASS):
 class DataStore(QtCore.QObject):
     #define signals
     fileCreated = QtCore.pyqtSignal()
+    projectLoaded = QtCore.pyqtSignal()
 
     def __init__(self):
         super(DataStore, self).__init__()
@@ -232,9 +276,11 @@ class DataStore(QtCore.QObject):
         self.drillholesXYZ={}
         
         #variables for running project
+        self.savefilepath = None
         self.projectdir = None
         self.projectname = None
         self.projectCRS = None  #consider initialising this to a real projection?
+        # a variable to store the list of section definition files used by section viewer?
         
         #variables important to calculations/backend
         self.collarfile = None
@@ -249,15 +295,58 @@ class DataStore(QtCore.QObject):
         self.planLogLayers = []
         self.existingLayersDict ={}
         
-    def createProjectFile(self):
-    #a function to create a project file to save settings etc to
-        print "this doesnt do anything yet"
-    def loadProject():
+    
+    def loadProjectData(self):
     #a function to load settings from a saved project file
-        empty=None  
-    def saveProject():
+        savefile = ET.parse(self.savefilepath)  #need to implement a openfile dialog in QDrillerDialog
+        root = savefile.getroot()
+
+        #assign variable from savefile datastore
+        self.projectdir = root.find("prjdir").text
+        self.projectname = root.find("prjname").text
+        self.collarfile = root.find("collar").text
+        self.surveyfile = root.find("survey").text
+        
+        selCRS = qgis.core.QgsCoordinateReferenceSystem()
+        #turn blank CRS into desired CRS using the authority identifier
+        crsid =root.find("prjcrs").text
+        selCRS.createFromUserInput(crsid)
+        #set the projectCRS to the selected CRS (this is a QgsCRS type object)
+        self.projectCRS = selCRS
+        
+       
+        self.logfiles = []
+        for logs in root.findall("log"):
+            self.logfiles.append(logs.text)
+            
+        self.existingLayersDict ={}
+        for exlayers in root.findall("existlyr"):
+            name = exlayers.get("name")
+            path = exlayers.text
+            self.existingLayersDict[name] = path
+        
+        #emit signal?
+        self.projectLoaded.emit()
+        
+    def saveProjectData(self):
     #a function to write current settings to current project file
-        empty=None
+        
+        root = ET.Element("savefile")
+        ET.SubElement(root, "prjdir").text = self.projectdir
+        ET.SubElement(root, "collar").text = self.collarfile
+        ET.SubElement(root, "survey").text = self.surveyfile
+        ET.SubElement(root, "prjname").text = self.projectname
+        ET.SubElement(root, "prjcrs").text = self.projectCRS.authid()
+        
+        for logs in self.logfiles:
+            ET.SubElement(root, "log").text = logs
+            
+        for k in self.existingLayersDict:
+            ET.SubElement(root, "existlyr",{"name":k}).text = self.existingLayersDict[k]
+        
+        savefile = ET.ElementTree(root)
+        savefile.write(self.savefilepath)
+        #also set up for sectionviewer to throw a list of section definition files to the datastore and be written here
         
     def calcDrillholes(self):
     #read in from file and create the drillhole arrays for use
@@ -317,7 +406,7 @@ class SectionView(QtGui.QMainWindow, SECT_FORM_CLASS):
         #self.sectionCanvas.setCanvasColour(Qt.white)
         self.lytMap.addWidget(self.sectionCanvas)
         self.crs= qgis.core.QgsCoordinateReferenceSystem()
-        self.crs.createFromProj4("+proj=utm +zone=54 +south +ellps=aust_SA +units=m +no_defs")
+        self.crs.createFromUserInput("EPSG:4978")
         self.sectionCanvas.setDestinationCrs(self.crs)
         self.sectionCanvas.setMapUnits(0)
         
@@ -431,53 +520,65 @@ class GenerateSection(QtGui.QDialog, GEN_FORM_CLASS):
         self.secAzi = None      #may need to investigate the maths behind this more to get facing
         self.secFacing = None
         self.envWidth = None
+        self.secLength = None
         self.holes2plotXYZ = None
         self.availLogs ={}
         self.selectedLogs = []
+        self.availDrillholes = []
+        self.selDrillholes = []
         
         
         
         
         #set the crs using the same hardcode as in the section viewer
         self.crs= qgis.core.QgsCoordinateReferenceSystem()
-        self.crs.createFromProj4("+proj=utm +zone=54 +south +ellps=aust_SA +units=m +no_defs")
+        self.crs.createFromUserInput("EPSG:4978")
         
         #connect GUI buttons
         self.btnAddDHLog.clicked.connect(self.addDHLog)
         self.btnRemDHLog.clicked.connect(self.remDHLog)
+        self.btnAddDH.clicked.connect(self.addDH)
+        self.btnRemDH.clicked.connect(self.remDH)
         self.btnDraw.clicked.connect(self.generateSection)
+        self.btnFilterHoles.clicked.connect(self.filterHoles)
         
         
         #connect GUI with variables
         self.ledSecName.textChanged.connect(lambda: self.setVars("name", self.ledSecName.text()))
-        self.ledOriginX.textChanged.connect(lambda: self.setVars("oringinX", self.ledOriginX.text()))
+        self.ledOriginX.textChanged.connect(lambda: self.setVars("originX", self.ledOriginX.text()))
         self.ledOriginY.textChanged.connect(lambda: self.setVars("originY", self.ledOriginY.text()))
-        self.ledAzi.textChanged.connect(lambda: self.setvars("azi", self.ledAzi.text()))
+        self.ledAzi.textChanged.connect(lambda: self.setVars("azi", self.ledAzi.text()))
         self.ledEnv.textChanged.connect(lambda: self.setVars("Env", self.ledEnv.text()))
+        self.ledSecLength.textChanged.connect(lambda: self.setVars("seclength", self.ledSecLength.text()))
         
         #populate lists
         self.populateDHloglist()
         
     def setVars(self, target, value):
         
-        if target == "originx":
-           self.originX = value
-        elif target == "originy":
-            self.originY = value
+        if target == "originX":
+           self.originX = float(value)
+        elif target == "originY":
+            self.originY = float(value)
         elif target == "azi":
-            self.secAzi = value
+            self.secAzi = float(value)
         elif target == "name":
-            self.SecName = value
+            self.secName = value
         elif target == "Env":
-            self.envWidth = value
+            self.envWidth = float(value)
+        elif target == "seclength":
+            self.secLength = value
             
     def subsetDrillholes(self):
         #create the subset of drill XYZ to plot
         self.holes2plotXYZ = QDrillerDialog.datastore.drillholesXYZ #placeholder only, need to make function to perform subset
         
+    def filterHoles(self):
+        #function to filter the available holes to that within the envelope and set the selected drillholes to this
+        print "filtering not implemented yet"
         
     def populateDHloglist(self):
-        
+        #generate a list of available downhole log datasets
         availLogNames =[]
         
         for entry in QDrillerDialog.datastore.logfiles:
@@ -488,18 +589,34 @@ class GenerateSection(QtGui.QDialog, GEN_FORM_CLASS):
         for item in availLogNames:
             self.lstAvailLogs.addItem(item)
             
+        #generate a list of available drillholes
+        for holes in QDrillerDialog.datastore.drillholesXYZ.keys():
+            self.lstAvailDH.addItem(holes)
+        
+            
     def addDHLog(self):
         print "addDHLog function called"
         for item in self.lstAvailLogs.selectedItems():
-            self.lstSelLogs.addItem(item.text())
+            self.lstSelLogs.addItem(self.lstAvailLogs.takeItem(self.lstAvailLogs.row(item)))
        
         
     def remDHLog(self):
         print "remDHLog function called"
         
         for item in self.lstSelLogs.selectedItems():
-            trash = self.lstSelLogs.takeItem(self.lstSelLogs.row(item))
-            trash = None
+            self.lstAvailLogs.addItem(self.lstSelLogs.takeItem(self.lstSelLogs.row(item)))
+            
+    def addDH(self):
+        print "addDH function called"
+
+        for item in self.lstAvailDH.selectedItems():
+            self.lstSelDH.addItem(self.lstAvailDH.takeItem(self.lstAvailDH.row(item)))
+        
+    def remDH(self):
+        print "remDH function called"
+
+        for item in self.lstSelDH.selectedItems():
+            self.lstAvailDH.addItem(self.lstSelDH.takeItem(self.lstSelDH.row(item)))
 
     def generateSection(self):
         #create directory to store the relevant section files
@@ -509,19 +626,21 @@ class GenerateSection(QtGui.QDialog, GEN_FORM_CLASS):
         self.subsetDrillholes()
         #create drill traces
         outputlayer = os.path.normpath(r"{}\{}\{}_traces_S.shp".format(
-                                        QDrillerDialog.datastore.projectdir,self.SecName,
+                                        QDrillerDialog.datastore.projectdir,self.secName,
                                         QDrillerDialog.datastore.projectname)
                                         )
         #create directory to store the relevant section files
         ### maybe at some point put in here a warning if the directory already exists?
-        sectiondirectory = os.path.dirname(outlayer)
+        sectiondirectory = os.path.dirname(outputlayer)
         try: 
             os.makedirs(sectiondirectory)
         except OSError:
             if not os.path.isdir(sectiondirectory):
                 raise
-
-        QDUtils.writeTraceLayer(self.holes2plotXYZ, outputlayer, loadcanvas=False, crs=self.crs)
+        secplane = [self.originX, self.originY, self.secAzi]
+        print "secplane", secplane
+            
+        QDUtils.writeTraceLayer(self.holes2plotXYZ, outputlayer, plan=False, sectionplane=secplane, loadcanvas=False, crs=self.crs)
         #layername = os.path.splitext(os.path.basename(outputlayer))[0]
        # self.existingLayersDict[layername]= outputlayer
        
@@ -533,16 +652,19 @@ class GenerateSection(QtGui.QDialog, GEN_FORM_CLASS):
         for i in xrange(self.lstSelLogs.count()):
             name = self.lstSelLogs.item(i).text()
             logtargetDict[name] = self.availLogs[name] 
-            
+        
+        print "logtargetDict", logtargetDict
         #code to generate log 
         for k in logtargetDict:
             
             logname = k
             logtarget = logtargetDict[k]
             outputlayer = os.path.normpath("{}\\{}\\{}_{}_S.shp".format(QDrillerDialog.datastore.projectdir,
-                                            QDrillerDialog.datastore.projectname, self.Secname, logname)
+                                            self.secName, QDrillerDialog.datastore.projectname, logname)
                                             )
+            print "outputlayer", outputlayer
             secplane = [self.originX, self.originY, self.secAzi]
+            print "secplane", secplane
             
             QDUtils.LogDrawer(self.holes2plotXYZ, logtarget, outputlayer, plan=False, 
                                 sectionplane=secplane, crs=self.crs, loadcanvas=False)
